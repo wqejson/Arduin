faf67926dce6d23c5bac60483f87dcc1340d2380
DanyEVO <daniel.valean2002@gmail.com>
2018-12-22 19:25:28 +0200

    changing text

--- Script/change_text.js
+++ Script/change_text.js
@@ -1,13 +1,12 @@
-var text = ["This is Pulse.", "Welcome", "Hi"];
-var counter = 0;
+var text = ["This is Pulse.", "Welcome.", "Hi."];
+var counter = 1;
 var elem = document.getElementById("changeText");
-var inst = setInterval(change, 5000);
+var inst = setInterval(change, 2000);
 
 function change() {
 	elem.innerHTML = text[counter];
 	counter++;
 	if (counter >= text.length) {
 		counter = 0;
-		//clearInterval(inst);
 	}
 }
